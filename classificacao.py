95ffb632106fb882e802815f862ff5cda1865d31
Jhonata Ribeiro <jhonatar@stj.jus.br>
2018-12-27 17:41:15 -0200

    adiciona mais objetos objetos para classificacao

--- classificacao.py
+++ classificacao.py
@@ -9,12 +9,16 @@ cachorro1 = [0, 1, 1]
 cachorro2 = [1, 1, 1]
 cachorro3 = [0, 0, 1]
 
-animal = [0, 0, 0]
-
 dados = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
 
 marcacoes = [1, 1, 1, -1, -1, -1]
 
+animalDesconhecido = [0, 0, 0]
+animalDesconhecido2 = [1, 1, 0]
+animalDesconhecido3 = [0, 1, 0]
+animalDesconhecido4 = [0, 0, 1]
+
 modelo = MultinomialNB()
 modelo.fit(dados, marcacoes)
-print(modelo.predict([animal]))
+print(modelo.predict(
+    [animalDesconhecido, animalDesconhecido2, animalDesconhecido3, animalDesconhecido4]))
